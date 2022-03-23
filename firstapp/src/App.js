b820981499e0bb1d3acea5ea4b799a99f06de4f5
pranjal <pranjalsmulane@gmail.com>
2022-03-23 09:25:37 +0530

    States - Text Form Created

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
     <>
-    <Navbar titleText="TextProject" aboutText="About us" contactText="Contact" />
-     
+    <Navbar titleText="React Text Analyzer" aboutText="About us" contactText="Contact" />
+     <TextForm/>
     </>
   );
 }
---
+++ firstapp/src/Components/TextForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+export default function TextForm() {
+
+    //use casedeclaration
+    const [text, setText] = useState('');
+
+    //function for uppercase button
+    const uppercaseClick = () => {
+        // console.log("sucess");
+        let newText = text.toUpperCase();
+        setText(newText);
+
+    }
+
+    //function for lowercase button
+    const lowercaseClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    //funtion to clear the text
+    const clearText = () => {
+        let newText = '';
+        setText(newText);
+
+    }
+
+    //event declare
+    const clickOnChange = (event) => {
+
+        setText(event.target.value);
+    }
+
+    return (
+        <>
+            <div className="container my-5">
+                <div className="form-group">
+                    <h2>Text Analyzer</h2>
+                    {/* text area JSX */}
+                    <textarea className="form-control" onChange={clickOnChange} value={text} id="textBox" rows={6} placeholder="Enter Your Text" />
+                </div>
+                {/* buttons JSX */}
+                <button className="btn btn-dark mx-2" onClick={uppercaseClick}>Convert to Uppercase</button>
+                <button className="btn btn-dark mx-2" onClick={lowercaseClick}>Convert to Lowercase</button>
+                <button className="btn btn-dark mx-2" onClick={clearText}>Clear Text</button>
+            </div>
+
+            {/* text summery */}
+            <div className="container">
+                <h4>Text Summery</h4>
+                <p>Total Words : {text.split(" ").length-1}</p>
+                <p>Total Characters : {text.length} </p>
+            </div>
+        </>
+
+
+    )
+}
